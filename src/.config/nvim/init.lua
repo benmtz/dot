8d2a2af9263796d1bf07625b3c56d6e96829b9bb
Ben MERTZ <ben.mertz.dev@gmail.com>
2023-01-29 23:40:25 +0100

    plugin cleanup

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -8,36 +8,8 @@ require("telescope-config")
 require("trouble").setup { }
 require("autocompletion")
 require("language-servers")
-require("neogit").setup {
-  -- Setting any section to `false` will make the section not render at all
-  sections = {
-    untracked = {
-      folded = false
-    },
-    unstaged = {
-      folded = false
-    },
-    staged = {
-      folded = false
-    },
-    stashes = {
-      folded = true
-    },
-    unpulled = {
-      folded = true
-    },
-    unmerged = {
-      folded = false
-    },
-    recent = {
-      folded = true
-    },
-  },
-}
-
+--
 -- Theming
 require("dashboard-setup")
 require("lualine-config")
-
-
 
