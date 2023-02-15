1542f98dbb27189a0043ff4697964eaccd88e521
Ben MERTZ <ben.mertz.dev@gmail.com>
2023-02-15 10:31:48 +0100

    feat(asdf): asdf autoinstall

--- src/.config/fish/config.fish
+++ src/.config/fish/config.fish
@@ -13,5 +13,11 @@ set PATH $HOME/go/bin $PATH
 
 direnv hook fish | source
 
+if not test -d $HOME/.asdf
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
+  mkdir -p ~/.config/fish/completions
+  ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+end
+source ~/.asdf/asdf.fish
 
 
