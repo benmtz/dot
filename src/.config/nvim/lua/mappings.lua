1e5453f6e35b84a11508018164cfd3b4f9b66e8d
Benjamin Mertz <benjamin.mertz@bens-MacBook-Pro.local>
2023-05-01 18:59:13 +0200

    format mappings lua file

--- src/.config/nvim/lua/mappings.lua
+++ src/.config/nvim/lua/mappings.lua
@@ -1,13 +1,13 @@
-vim.g.mapleader=" "
-vim.api.nvim_set_keymap('n', '<SPACE>', '<Nop>', {noremap = true})
+vim.g.mapleader = " "
+vim.api.nvim_set_keymap('n', '<SPACE>', '<Nop>', { noremap = true })
 
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w><C-j>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w><C-h>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w><C-k>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w><C-l>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w><C-j>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w><C-h>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w><C-k>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w><C-l>', { noremap = true })
 
-vim.api.nvim_set_keymap('t', '<A-ESC>', '<C-\\><C-n>', {noremap = true})
-vim.api.nvim_set_keymap('t', '<F12>', '<C-\\><C-n><ESC>', {noremap = true})
+vim.api.nvim_set_keymap('t', '<A-ESC>', '<C-\\><C-n>', { noremap = true })
+vim.api.nvim_set_keymap('t', '<F12>', '<C-\\><C-n><ESC>', { noremap = true })
 
 local wk = require("which-key")
 wk.register({
@@ -17,8 +17,8 @@ wk.register({
     },
     t = {
       name = "Tabs",
-      e = { "<cmd>tabe<cr>", "New tab"},
-      n = { "<cmd>tabn<cr>", "Next tab"},
+      e = { "<cmd>tabe<cr>", "New tab" },
+      n = { "<cmd>tabn<cr>", "Next tab" },
       p = { "<cmd>tabp<cr>", "Previous tab" },
     },
     i = {
@@ -29,19 +29,18 @@ wk.register({
     },
     z = {
       name = "Terminal",
-      x = {'<cmd>15sp term://fish<cr>', 'Term in split'},
-      v = {'<cmd>vsp term://fish<cr>', 'Term in vsplit' },
-      e = {'<cmd>e term://fish<cr>', 'Term in buffer' },
-      t = {'<cmd>tabe term://fish<cr>', 'Term in tab' }
+      x = { '<cmd>15sp term://fish<cr>', 'Term in split' },
+      v = { '<cmd>vsp term://fish<cr>', 'Term in vsplit' },
+      e = { '<cmd>e term://fish<cr>', 'Term in buffer' },
+      t = { '<cmd>tabe term://fish<cr>', 'Term in tab' }
     },
     w = {
       name = "External Apps",
-      g = {'<cmd>FloatermNew --width=0.9 --height=0.9 lazygit<cr>', 'lazygit'},
-      e = {'<cmd>FloatermNew --width=0.9 --height=0.9 ranger<cr>', 'ranger'},
-      k = {'<cmd>FloatermNew --width=0.9 --height=0.9 k9s<cr>', 'k9s'},
-      t = {'<cmd>FloatermNew taskwarrior-tui<cr>', 'taskwarrior'}
+      g = { '<cmd>FloatermNew --width=0.9 --height=0.9 lazygit<cr>', 'lazygit' },
+      e = { '<cmd>FloatermNew --width=0.9 --height=0.9 ranger<cr>', 'ranger' },
+      k = { '<cmd>FloatermNew --width=0.9 --height=0.9 k9s<cr>', 'k9s' },
+      t = { '<cmd>FloatermNew taskwarrior-tui<cr>', 'taskwarrior' }
     },
-
     c = {
       name = "Code",
       gD = {'<cmd>lua vim.lsp.buf.declaration()<CR>', 'Go to declaration'},
@@ -60,21 +59,20 @@ wk.register({
     },
     f = {
       name = "Find",
-      h = { '<cmd>lua require("telescope.builtin").git_files()<cr>', 'Find file (.gitignored)'},
-      f = { '<cmd>lua require("telescope.builtin").find_files({follow = true})<cr>', 'Find file (All)' },
+      f = { '<cmd>lua require("telescope.builtin").git_files()<cr>', 'Find file (.gitignored)' },
+      a = { '<cmd>lua require("telescope.builtin").find_files({follow = true})<cr>', 'Find file (All)' },
       m = { '<cmd>lua require("telescope.builtin").marks()<cr>', 'Find mark' },
       g = { '<cmd>lua require("telescope.builtin").live_grep()<cr>', 'Find text (grep)' },
       e = { '<cmd>Telescope file_browser<cr>', 'File browser' },
-      b = { '<cmd>lua require("telescope.builtin").buffers()<cr>' , 'Find browser'},
-      r = { '<cmd>lua require("telescope").extensions.asynctasks.all()<cr>', 'Find asynctask'},
-      p = { '<cmd>lua require("telescope").extensions.project.project{}<cr>', 'Find project'}
+      b = { '<cmd>lua require("telescope.builtin").buffers()<cr>', 'Find browser' },
+      r = { '<cmd>lua require("telescope").extensions.asynctasks.all()<cr>', 'Find asynctask' },
+      p = { '<cmd>lua require("telescope").extensions.project.project{}<cr>', 'Find project' }
     },
     y = {
       name = "Yank",
       p = { '<cmd>let @+=expand("%")<cr>', 'Yank path' },
-      g = { '<cmd>let @+=system("git-remote-url " . expand("%") . " -l " . line("."))<cr>', 'Yank git path'},
+      g = { '<cmd>let @+=system("git-remote-url " . expand("%") . " -l " . line("."))<cr>', 'Yank git path' },
       b = { [[ mzggVG"+y'z ]], 'Yank buffer' },
     }
   }
 })
-
