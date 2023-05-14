223a11ee4cbc8f6f5d4df6430addfe5b5f3eaa36
Ben <ben.mertz.dev@gmail.com>
2023-05-14 10:41:20 +0200

    linux fish brew

--- src/.config/fish/config.fish
+++ src/.config/fish/config.fish
@@ -8,6 +8,10 @@ if status is-interactive
   alias ansibleLint="docker run -v $INFRA_MANAGEMENT_DIR/ansible:/sources quay.io/ansible/creator-ee:latest bash -c 'cd /sources; ansible-lint --offline -f md --force-color'"
 end
 
+if test -d /home/linuxbrew/.linuxbrew 
+	eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
+
 set PATH $HOME/.cargo/bin $PATH
 set PATH $HOME/go/bin $PATH
 set PATH /opt/homebrew/opt/openjdk/bin $PATH
