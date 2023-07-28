daa89c2a1da468214e3f627640c8cf51f1be4f7a
Ben MERTZ <ben.mertz.dev@gmail.com>
2023-07-29 00:12:13 +0200

    feat(termux): enhance install script

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 REPO_URL=https://github.com/benmtz/dot.git
 DEST=~/.dots
 
-pkg install -y python git jq
+pkg upgrade -y
+pkg install -y python git jq ranger lazygit nodejs golang starship
 
 git clone $REPO_URL $DEST 
 cd $DEST 
