b521a8a31e688540ebf4afade9fe5e219409098d
Ben MERTZ <ben.mertz.dev@gmail.com>
2023-07-29 21:10:47 +0200

    refactor(termux): rename install script

---
+++ install_termux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+REPO_URL=https://github.com/benmtz/dot.git
+DEST=~/.dots
+
+# shellcheck disable=SC2054
+packages=(
+  python
+  git
+  jq
+  ranger
+  lazygit
+  nodejs
+  golang
+  starship
+)
+
+pkg upgrade -y
+pkg install -y "${packages[@]}"
+
+python3 -m pip install lolcat
+
+rm -f /data/data/com.termux/files/usr/etc/motd
+
+
+# git clone $REPO_URL $DEST 
+# cd $DEST 
+# 
+# pip install -r requirements.txt
+# 
+# python dotfiles.py install-font
+# python dotfiles.py compile
+# python dotfiles.py link --force
+# 
