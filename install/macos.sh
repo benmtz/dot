660864e7ad51f51515587480bddbe44c5965de84
Ben MERTZ <you@example.com>
2023-07-30 20:27:46 +0200

    feat(macos): install script

---
+++ install/macos.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+packages=(
+  # Term utilities
+  rg
+  ranger
+  starship
+  tree
+  jq
+  yq
+  neovim
+  lazygit
+  tmux
+  # Lang
+  go
+  node
+  deno
+  # IaC
+  ansible-lint
+  ansible
+  packer
+  terraform
+  age
+  sops
+  dive
+  govc
+  yamllint
+  # Kube
+  k9s
+  k3sup
+  stern
+  kubie
+  fish
+)
+
+brew install "${packages[@]}"
+
+sudo chsh -s /usr/local/bin/fish $(whoami)
+
+python3 -m pip install lolcat
+
