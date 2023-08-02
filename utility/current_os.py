90c48ce7c3c2acffdd7fca6ea8bd908e7e12517b
Ben MERTZ <you@example.com>
2023-08-02 17:49:27 +0200

    feat(macos): add darwin detection

--- utility/current_os.py
+++ utility/current_os.py
@@ -1,9 +1,12 @@
 import os
 from typing import Literal, Optional
+from sys import platform
 
 def get_current_os() -> Optional[Literal["termux","linux","macos"]]:
     if os.getenv("TERMUX_VERSION"):
         return "termux"
+    elif platform == "darwin":
+        return "macos"
     else:
         return "linux"
 
