bd05ae332872ac0b03b378632c5ca00f238ef9db
Ben MERTZ <benjamin.mertz@techsoft3d.com>
2024-02-17 08:32:32 +0100

    feat: disable lolcat if the command is not available

--- src/.config/fish/functions/fish_greeting.fish
+++ src/.config/fish/functions/fish_greeting.fish
@@ -1,7 +1,11 @@
 if set -q USE_CUSTOM_GREETER;
   function fish_greeting
     set file $(random choice ~/.config/fish/greeters/*)
-    cat $file | sed -r "s/(.{0,$(tput cols)}).*/\1/" | lolcat
+    if type -q lolcat
+      cat $file | sed -r "s/(.{0,$(tput cols)}).*/\1/" | lolcat
+    else
+      cat $file | sed -r "s/(.{0,$(tput cols)}).*/\1/"
+    end
   end
 end
 
