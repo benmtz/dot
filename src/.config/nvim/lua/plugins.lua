88d1d46ab3cc97902a83c07769001a1b8301b698
Ben MERTZ <benjamin.mertz@techsoft3d.com>
2024-06-15 13:52:14 +0200

    feat: migrat dependency management to lazy.nvim

--- src/.config/nvim/lua/plugins.lua
+++ src/.config/nvim/lua/plugins.lua
@@ -1,94 +1,88 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
-return require('packer').startup(function()
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  --  INIT plugins --
+	{ 'nvim-treesitter/nvim-treesitter', lazy = false, priority = 1000 },
+  { 'folke/tokyonight.nvim', lazy = false, priority = 1000,
+      config = function()
+        require("tokyonight").setup({
+          style = "night",
+          transparent = true,
+          floats = "dark",
+          on_colors = function(colors)
+            -- colors.fg_gutter = "#5081c0"
+            colors.comment = "#709db2"
+            colors.dark5 = "#709db2"
+            colors.border = "#fefefe"
+            colors.bg_float = colors.none
+          end,
+          on_highlights = function(hl, colors)
+            hl.LineNr = {
+              fg = "#ffba00"
+              -- fg = "#5081c0"
+            }
+            hl.qfLineNr = {
+              -- fg = "#737aa2"
+              fg = "#ffba00"
+            }
+            hl.CursorLineNr = {
+              fg = "#ffba00"
+            }
+          end
+        })
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+	{ 'norcalli/nvim-colorizer.lua' },
+	{ 'folke/which-key.nvim', lazy = true },
+  { 'sheerun/vim-polyglot' },
+  { 'saadparwaiz1/cmp_luasnip' },
+  { 'voldikss/vim-floaterm' },
+  { 'nvim-telescope/telescope.nvim',
+    dependencies = {
+ 		  {'nvim-lua/plenary.nvim'},
+ 			{'nvim-telescope/telescope-file-browser.nvim'},
+ 			{ 'benmtz/telescope-go-task.nvim' }
+    }
+  },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "neovim/nvim-lspconfig" },
+  { 'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'dcampos/cmp-snippy',
+      'dcampos/nvim-snippy',
+    }
+  },
+  { 'rmagatti/auto-session',
+     lazy = false,
+     priority = 1000,
+     config = function()
+       require("auto-session").setup {
+         log_level = "error",
+         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+       }
+     end
+  },
+	{ 'terrortylor/nvim-comment' },
+  { "lukas-reineke/indent-blankline.nvim" },
+  { "folke/trouble.nvim", dependencies = { "kyazdani42/nvim-web-devicons" } },
+  { 'ckipp01/nvim-jenkinsfile-linter', dependencies = { "nvim-lua/plenary.nvim" } },
+  { 'nvim-lualine/lualine.nvim', dependencies = { 'kyazdani42/nvim-web-devicons' } }
+})
 
-  use 'wbthomason/packer.nvim'
-
-	use 'nvim-treesitter/nvim-treesitter'
-	use 'norcalli/nvim-colorizer.lua'
-
-	use {
-		'rmagatti/auto-session',
-		config = function()
-			require("auto-session").setup {
-				log_level = "error",
-				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-			}
-		end
-	}
-
-	use 'sheerun/vim-polyglot'
-
-	use {
-    "folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-	}
-
-	use {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"neovim/nvim-lspconfig",
-	}
-
-	use {
-		'hrsh7th/nvim-cmp',
-		requires = {
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-cmdline',
-			'dcampos/cmp-snippy',
-			'dcampos/nvim-snippy',
-		}
-	}
-
-	-- Jenkins linting
-	use({'ckipp01/nvim-jenkinsfile-linter', requires = { "nvim-lua/plenary.nvim" } })
-
-	-- Telescope
-	use {
-	  'nvim-telescope/telescope.nvim',
-	  requires = {
-		  {'nvim-lua/plenary.nvim'},
-			{'nvim-telescope/telescope-file-browser.nvim'},
-			-- {'~/Code/ben/telescope-go-task.nvim'}
-			{ 'benmtz/telescope-go-task.nvim' }
-	  }
-	}
-
-	use 'saadparwaiz1/cmp_luasnip'
-
-	-- Comments
-	use "terrortylor/nvim-comment"
-
-	-- Task management
-	use 'skywind3000/asynctasks.vim'                           -- Task management system
-	use 'skywind3000/asyncrun.vim'
-	use 'GustavoKatel/telescope-asynctasks.nvim'
-
-	-- UI plugins
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
-	use {
-		'glepnir/dashboard-nvim',
-		requires = {'nvim-tree/nvim-web-devicons'}
-	}
-	use 'folke/tokyonight.nvim'
-
-	use 'voldikss/vim-floaterm'
-
-	use "lukas-reineke/indent-blankline.nvim"
-
-	use "folke/which-key.nvim"
-
-	if packer_bootstrap then
-    require('packer').sync()
-  end
-
-end)
