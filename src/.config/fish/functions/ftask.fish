576b51b443e132d65576654cb493d1ac7961edd8
Ben MERTZ <benjamin.mertz@techsoft3d.com>
2024-06-29 13:24:16 +0200

    Update task execution in ftask.fish
    The function ftask.fish script has been updated to not only execute a selected task but also display the task's name with prefix and suffix "--= task [taskname] =--". This makes it easier for users to identify which task is currently being executed.

--- src/.config/fish/functions/ftask.fish
+++ src/.config/fish/functions/ftask.fish
@@ -4,7 +4,8 @@ function ftask
 
   set result $(task --list-all | sed -e '1d; s/\* \(.*\):\s*\(.*\)\s*(aliases.*/\1\t\2/' -e 's/\* \(.*\):\s*\(.*\)/\1\t\2/' | awk '{$1= $1};1' | fzf)
   if test -n $result
-    task -f $result
+    task $result
+    echo "--= task $result =--"
   end
 
   if set -ql _flag_loop
