bc256c51394d62c06180921c82460417dce25de6
Ben MERTZ <benjamin.mertz@techsoft3d.com>
2025-04-13 09:27:04 +0200

    comment auto_sessions

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -31,17 +31,17 @@ require'colorizer'.setup()
 
 require("lualine-config")
 
-require("auto-session").setup {
-  log_level = "error",
-  post_restore_cmds = {
-    "bd copilot-chat"
-  },
-  cwd_change_handling = {
-    restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
-    pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
-    post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
-      require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
-    end,
-  },
-}
+-- require("auto-session").setup {
+--   log_level = "error",
+--   post_restore_cmds = {
+--     "bd copilot-chat"
+--   },
+--   cwd_change_handling = {
+--     restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
+--     pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
+--     post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+--       require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+--     end,
+--   },
+-- }
 
--- src/.config/nvim/lua/plugins/auto_sessions.lua
+++ src/.config/nvim/lua/plugins/auto_sessions.lua
@@ -1,12 +1,12 @@
-return {
-  { 'rmagatti/auto-session',
-     lazy = false,
-     priority = 1000,
-     config = function()
-       require("auto-session").setup {
-         log_level = "error",
-         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-       }
-     end
-  }
+ return {
+--   { 'rmagatti/auto-session',
+--      lazy = false,
+--      priority = 1000,
+--      config = function()
+--        require("auto-session").setup {
+--          log_level = "error",
+--          auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+--        }
+--      end
+--   }
 }
