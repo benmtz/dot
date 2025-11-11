e2382e8d8162b6942a180df0d45e5a12d987264f
Ben MERTZ <ben.mertz.dev@gmail.com>
2025-11-11 15:03:27 +0100

    no extraneous logs if dir does not exists in add-path

--- src/.config/zsh/functions/002_add-path.zsh
+++ src/.config/zsh/functions/002_add-path.zsh
@@ -1,10 +1,7 @@
 registr add add-path "Add a directory to the PATH environment variable" "add-path ~/.local/bin"
 function add-path() {
-
   if [[ -d "$1" ]]; then
     export PATH="$1:$PATH"
-  else
-    echo "Directory $1 does not exist"
   fi
 }
 
