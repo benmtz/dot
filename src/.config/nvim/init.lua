6535bade20225013d561bbe9d71c38cf01fa315e
Ben MERTZ <benjamin.mertz@techsoft3d.com>
2026-04-23 21:24:12 +0200

    Remove dead code

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -4,7 +4,6 @@ require("mappings")
 require("global-settings")
 require("telescope-config")
 
-
 -- Plugin specific configurations
 -- Development utilities
 require("trouble").setup { }
@@ -14,18 +13,4 @@ require("language-servers")
 require'colorizer'.setup()
 
 require("lualine-config")
-
--- require("auto-session").setup {
---   log_level = "error",
---   post_restore_cmds = {
---     "bd copilot-chat"
---   },
---   cwd_change_handling = {
---     restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
---     pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
---     post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
---       require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
---     end,
---   },
--- }
 
